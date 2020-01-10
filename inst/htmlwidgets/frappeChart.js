b19e33af8fdca579a8578bcd7a39c6d1e43fb32c
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 09:09:12 -0500

    refactor data processing function

--- inst/htmlwidgets/frappeChart.js
+++ inst/htmlwidgets/frappeChart.js
@@ -8,28 +8,30 @@ HTMLWidgets.widget({
 
     // TODO: define shared variables for this instance
 
+    // helper function to prep the data
+    const prepareChartData = function(data) {
+      const chartData = {labels: [], datasets: []}
+
+      // Get keys of data, assume that first entry is for labels, the rest are data
+      let labelColumn = Object.keys(data)[0]
+      let columns = Object.keys(data).slice(1)
+
+      // First column in x.data is the labels
+      chartData.labels = data[labelColumn]
+
+      // Create an appropriate object for each column, reformat data and add to chartData
+      columns.forEach(function(col) {
+        chartData.datasets.push({name: col, values: data[col]})
+      })
+      return chartData
+    }
+
     return {
 
       renderValue: function(x) {
-
-        const chartData = {labels: [], datasets: []}
-
-        // Get keys of data, assume that first entry is for labels, the rest are data
-        let labelColumn = Object.keys(x.data)[0]
-        let columns = Object.keys(x.data).slice(1)
-
-        // First column in x.data is the labels
-        chartData.labels = x.data[labelColumn]
-
-        // Create an appropriate object for each column, reformat data and add to chartData
-        columns.forEach(function(col) {
-          chartData.datasets.push({name: col, values: x.data[col]})
-        })
-
-        x.data = chartData
+        x.data = prepareChartData(x.data)
 
         const chart = new frappe.Chart(el, x)
-
       },
 
       resize: function(width, height) {
