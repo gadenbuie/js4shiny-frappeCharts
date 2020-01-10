6f141c4341a2c4f8615df81887e7927d2e765f11
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 03:16:03 +0000

    Add js binding: it's a widget!

--- inst/htmlwidgets/frappeChart.js
+++ inst/htmlwidgets/frappeChart.js
@@ -12,8 +12,23 @@ HTMLWidgets.widget({
 
       renderValue: function(x) {
 
-        // TODO: code to render the widget, e.g.
-        el.innerText = x.message;
+        const chartData = {labels: [], datasets: []}
+
+        // Get keys of data, assume that first entry is for labels, the rest are data
+        let labelColumn = Object.keys(x.data)[0]
+        let columns = Object.keys(x.data).slice(1)
+
+        // First column in x.data is the labels
+        chartData.labels = x.data[labelColumn]
+
+        // Create an appropriate object for each column, reformat data and add to chartData
+        columns.forEach(function(col) {
+          chartData.datasets.push({name: col, values: x.data[col]})
+        })
+
+        x.data = chartData
+
+        const chart = new frappe.Chart(el, x)
 
       },
 
