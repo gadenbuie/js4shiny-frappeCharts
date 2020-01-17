b7108029b58635652ce87f3e1ea9a2c5a6232020
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-17 09:32:23 -0500

    Create our own typingSpeedInput()

--- inst/shiny-input-app/app.R
+++ inst/shiny-input-app/app.R
@@ -1,7 +1,19 @@
 library(shiny)
 
+typingSpeedInput <- function(inputId, label, placeholder = NULL) {
+  .label <- label
+  htmltools::withTags(
+    div(
+      class = "form-group typing-speed",
+      label(class = "control-label", `for` = inputId, .label),
+      textarea(id = inputId, class = "form-control", placeholder = placeholder)
+    )
+  )
+}
+
 ui <- fluidPage(
-  textAreaInput("typing", "Type here..."),
+  # textAreaInput("typing", "Type here..."),
+  typingSpeedInput("typing", "Type here..."),
   verbatimTextOutput("debug")
 )
 
